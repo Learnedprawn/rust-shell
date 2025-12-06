a9f26fb8c883fd6151fc87d32038d5f4d1bf084e
Learnedprawn <vineshparalkar@gmail.com>
2025-12-06 13:15:21 +0530

    type command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+enum CommandType {
+    Builtin,
+    File,
+    NotFound,
+}
+
+impl CommandType {
+    pub fn from_str(s: &str) -> Self {
+        match s {
+            "exit" => CommandType::Builtin,
+            "echo" => CommandType::Builtin,
+            "type" => CommandType::Builtin,
+            _ => CommandType::NotFound,
+        }
+    }
+}
+
 fn main() {
     loop {
         print!("$ ");
@@ -22,6 +39,17 @@ fn main() {
                     print!("{} ", word);
                 }
                 print!("\n");
+            }
+            "type" => {
+                let type_command = input_iterator.next().expect("type command error");
+                match CommandType::from_str(type_command) {
+                    CommandType::Builtin => {
+                        println!("{} is a shell builtin", type_command)
+                    }
+                    _ => {
+                        println!("{} not found", type_command)
+                    }
+                }
             }
             _ => {
                 print!("{}: command not found\n", command.trim());
