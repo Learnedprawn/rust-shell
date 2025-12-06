a08fdc4a790f5d4e85fa910a43c64f92f54db0ec
Learnedprawn <vineshparalkar@gmail.com>
2025-12-06 11:56:23 +0530

    command not found

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,10 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+
+    print!("{}: command not found", command);
+
     io::stdout().flush().unwrap();
 }
