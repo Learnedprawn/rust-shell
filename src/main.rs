72509738baf0f9c93d712ccbeba1842941324c8b
Learnedprawn <vineshparalkar@gmail.com>
2025-12-06 12:02:11 +0530

    trim and flush

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
+    io::stdout().flush().unwrap();
+
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
 
-    print!("{}: command not found", command);
-
-    io::stdout().flush().unwrap();
+    print!("{}: command not found", command.trim());
 }
