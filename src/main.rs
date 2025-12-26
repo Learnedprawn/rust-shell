edfde82646808ee9da6fafe39a1afee97048ea22
Learnedprawn <vineshparalkar@gmail.com>
2025-12-26 18:37:48 +0530

    works

--- src/main.rs
+++ src/main.rs
@@ -88,10 +88,10 @@ fn main() {
         let mut in_double_quotes = false;
         let mut backslashed = false;
         for character in input.chars() {
-            // println!(
-            //     "character: {}, in_double_quotes: {}, backslashed: {}",
-            //     character, in_double_quotes, backslashed
-            // );
+            println!(
+                "character: {}, in_double_quotes: {}, backslashed: {}",
+                character, in_double_quotes, backslashed
+            );
             if backslashed {
                 current_buffer.push(character);
                 backslashed = false;
@@ -102,10 +102,11 @@ fn main() {
                     current_buffer.push(character);
                     continue;
                 }
-                if in_double_quotes {
-                    backslashed = true;
-                    continue;
-                }
+                // if in_double_quotes {
+                //     backslashed = true;
+                //     continue;
+                // }
+                backslashed = true;
                 continue;
             }
             if character == '\'' && !in_double_quotes {
